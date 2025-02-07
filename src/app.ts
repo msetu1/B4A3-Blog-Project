0193ac4180de1bbfb86da5825c7a769500cde7b0
msetu1 <msetu5763@gmail.com>
2025-02-07 21:59:47 +0600

    not found and validate request functionality done

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
 import globalErrorHandler from './app/middlewares/globalErrorHandler';
+import notFound from './app/middlewares/notFound';
 
 const app: Application = express();
 
@@ -13,12 +14,13 @@ app.use(cors());
 
 // Test route
 const test = async (req: Request, res: Response) => {
-  res.send('Blog project server is running');
+  res.send('Blog project server is running !');
 };
 
 app.get('/', test);
 
 // Error handlers
 app.use(globalErrorHandler);
+app.use(notFound);
 
 export default app;
